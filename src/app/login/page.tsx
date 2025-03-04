63646061692d4e14af91cd906e0a7bef26ee10f8
Andrey Rocha <ahrocha@gmail.com>
2025-03-04 11:35:16 -0300

    Login form.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { userLogin } from "@/services/api";
 
 export default function LoginPage() {
